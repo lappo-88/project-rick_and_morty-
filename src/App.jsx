55e45f2612b63cbb32e5346582ebb961f9a7c6c8
lappo-88 <mikhail08062025@gmail.com>
2025-08-05 22:20:58 +0300

    extreme addition

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage.jsx";
 import {CharacterPage} from "./pages/CharacterPage/CharacterPage.jsx";
 import {Header} from "./common/components/Header.jsx";
-import {Routes,Route} from "react-router";
+import {Routes, Route} from "react-router";
+import {Character} from "./pages/CharacterPage/Character/Character.jsx";
 
 function App() {
     return (
-    <div>
-        <Header/>
-        <Routes>
-            < Route path = "/" element={ <HomePage/>}/>
-            < Route path = "/characters" element={ <CharacterPage/>}/>
-            < Route path = "/locations" element={ <LocationPage/>}/>
-            < Route path = "/episodes" element={ <EpisodePage />}/>
-        </Routes>
-    </div>
+        <div>
+            <Header/>
+            <Routes>
+                < Route path="/" element={<HomePage/>}/>
+                < Route path="/characters" element={<CharacterPage/>}/>
+                <Route path="/characters/:id" element={<Character/>}/>
+                < Route path="/locations" element={<LocationPage/>}/>
+                < Route path="/episodes" element={<EpisodePage/>}/>
+            </Routes>
+        </div>
     )
 }
 
--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,9 +1,11 @@
 import s from "./CharacterPage.module.css"
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Link} from "react-router";
 
 export const CharacterPage = () => {
     const [characters, setCharacters] = useState([])
+    const [error, setError] = useState(null)
 
     const [info, setInfo] = useState({
         count: 0,
@@ -16,7 +18,12 @@ export const CharacterPage = () => {
         axios.get(url).then((res) => {
             setCharacters(res.data.results)
             setInfo(res.data.info)
+            setError(null)
+
         })
+            .catch((err) => {
+                setError(err.response.data.error)
+            })
     }
 
     useEffect(() => {
@@ -31,17 +38,27 @@ export const CharacterPage = () => {
         fetchData(info.prev)
     }
 
+    const searchHandler = (event) => {
+        const value = event.currentTarget.value;
+        fetchData(`https://rickandmortyapi.com/api/character?name=${value}`)
+    }
+
     return (
         <div className={"pageContainer"}>
             <h1 className={"pageTitle"}>CharacterPage</h1>
-            {characters.length && (
+            <input type="search" className={s.search} onChange={searchHandler} placeholder="Search..."/>
+            {error && <div className="errorMessage">{error }</div>}
+            {!error && characters.length && (
                 <>
                     {
                         <div className={s.characters}>
                             {characters.map((character) => {
                                 return (
                                     <div key={character.id} className={s.character}>
-                                        <div className={s.characterLink}>{character.name}</div>
+                                        {/*<div className={s.characterLink}>{character.name}</div>*/}
+                                        <Link to={`/characters/${character.id}`} className={s.characterLink}>
+                                            {character.name}
+                                        </Link>
                                         <img src={character.image} alt={`${character.name} avatar`}/>
                                     </div>
                                 )
